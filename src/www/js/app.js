6b0f79779b16fb8c17b6c1373a87a1f7a540e0be
Eric W. Greene <eric@ericwgreene.com>
2016-07-12 08:10:38 -0700

    day 1 code demos

--- src/www/js/app.js
+++ src/www/js/app.js
@@ -1,15 +1,15 @@
-import 'bootstrap-loader';
-import '../css/styles.scss';
+// import 'bootstrap-loader';
+// import '../css/styles.scss';
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-class AppComponent extends React.Component {
+// class AppComponent extends React.Component {
 
-	render() {
-		return <h1>React/Relay/GraphQL Starter Project!!!</h1>;
-	}
+// 	render() {
+// 		return <h1>React/Relay/GraphQL Starter Project!!!</h1>;
+// 	}
 
-}
+// }
 
-ReactDOM.render(<AppComponent />, document.querySelector('main'));
+// ReactDOM.render(<AppComponent />, document.querySelector('main'));
