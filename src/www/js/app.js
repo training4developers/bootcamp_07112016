d04dd25624d1de66ecbdb4bf9a243d75370e1c54
Eric W. Greene <eric@ericwgreene.com>
2016-07-12 16:51:27 -0700

    day 2 code demos

--- src/www/js/app.js
+++ src/www/js/app.js
@@ -1,15 +1,59 @@
 // import 'bootstrap-loader';
-// import '../css/styles.scss';
+import '../css/styles.scss';
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
-// class AppComponent extends React.Component {
-
-// 	render() {
-// 		return <h1>React/Relay/GraphQL Starter Project!!!</h1>;
-// 	}
-
+// function mul(x,y) {
+// 	return x * y;
 // }
 
-// ReactDOM.render(<AppComponent />, document.querySelector('main'));
+// const mul = (x,y) => x * y;
+
+// console.log(mul(1,2));
+
+// (function() {
+// 	console.log('do something');
+// })();
+
+// import React from 'react';
+
+// import { doIt as somethingElse } from './my-module';
+
+//myVeryFirstFunction();
+
+// console.log(somethingElse);
+
+function Person(fn, ln) {
+	this.fn = fn;
+	this.ln = ln;
+
+}
+
+var p = new Person('Bob','Martin');
+
+class Person2 {
+
+	constructor(fn, ln) {
+		this.fn = fn;
+		this.ln = ln;
+	}
+
+	getFullName() {
+		return this.fn + ' ' + this.ln;
+	}
+
+}
+
+class Student extends Person2 {
+	constructor(id, fn, ln) {
+		super(fn, ln);
+		this.id = id;
+	}
+}
+
+var p = new Person2('Jim','Stevens');
+console.log(p.getFullName());
+
+var s = new Student(1, 'Jim','Stevens');
+console.log(s.getFullName());
+
+console.log(typeof Person);
+console.log(typeof Person2);
---
+++ src/www/js/my-module.js
@@ -0,0 +1,7 @@
+export const myVeryFirstFunction = () => {
+	 console.log('test!');
+};
+
+export const doIt = 4;
+
+//export default 
